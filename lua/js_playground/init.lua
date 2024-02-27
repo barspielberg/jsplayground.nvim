3e24d7682c611b7cb2d320286a9fc3420cc643d6
bar spielberg <bar@testim.io>
2024-02-27 18:32:11 +0200

    toggle

--- lua/js_playground/init.lua
+++ lua/js_playground/init.lua
@@ -8,22 +8,33 @@ local opts = {
 	cmd = { "node" },
 }
 
----@type Runner|nil
+---@type Runner | nil
 local runner = nil
 --TODO:support multiple runners
 
-local M = {}
-
-M.start = function()
+local function stop()
 	if runner then
 		runner:detach()
 		runner = nil
+	end
+end
+
+local function listen_cur_buf()
+	vim.api.nvim_buf_attach(0, false, { on_detach = stop }) --FIXME:
+	local cur_dir = vim.fn.expand("%:p:h")
+	local file_name = vim.fn.expand("%:t")
+	local cmd = utils.create_cmd(opts.cmd, file_name)
+	runner = Runner.new(Console.new(opts.percentage))
+	runner:attach(cmd, cur_dir)
+end
+
+local M = {}
+
+M.toggle = function()
+	if runner then
+		stop()
 	else
-		local cur_dir = vim.fn.expand("%:p:h")
-		local file_name = vim.fn.expand("%:t")
-		local cmd = utils.create_cmd(opts.cmd, file_name)
-		runner = Runner.new(Console.new(opts.percentage))
-		runner:attach(cmd, cur_dir)
+		listen_cur_buf()
 	end
 end
 
--- plugin/init.lua
+++ plugin/init.lua
@@ -1,1 +1,1 @@
-vim.api.nvim_create_user_command("JSPlayground", require("js_playground").start, {})
+vim.api.nvim_create_user_command("JSPlayground", require("js_playground").toggle, {})
