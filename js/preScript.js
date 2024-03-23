8274f9ccd653dade9b3a0125ca11d669682c9168
bar spielberg <bar@testim.io>
2024-03-23 11:10:45 +0200

    Override list

--- js/preScript.js
+++ js/preScript.js
@@ -1,5 +1,9 @@
+const override = ['log', 'error', 'warn', 'info', 'debug'];
 console = new Proxy(console, {
   get: function (target, prop) {
+    if (!override.includes(prop)) {
+      return Reflect.get(...arguments)
+    }
     return function (...args) {
       const lineNum = new Error().stack.split("\n")[2].split(":")[1];
       const data = `line:${lineNum},prop:${prop}|`;
