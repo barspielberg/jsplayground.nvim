87d597807bfc390f282d0bd6d51ffc9db87aa9d6
Bar Spielberg <barspielberg@gmail.com>
2024-09-15 19:17:17 +0300

    better regex

--- js/preScript.js
+++ js/preScript.js
@@ -5,7 +5,7 @@ console = new Proxy(console, {
       return Reflect.get(...arguments)
     }
     return function (...args) {
-      const [_,lineNum] = new Error().stack.split("\n")[2].match(/:(\d+):(\d+)$/);
+      const [_,lineNum] = new Error().stack.split("\n")[2].match(/:(\d+):(\d+)\)?$/);
       const data = `line:${lineNum},prop:${prop}|`;
       target[prop].call(target, data, ...args);
     };
